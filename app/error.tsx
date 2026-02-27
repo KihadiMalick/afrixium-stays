833f00d98e65cb1d61aad8a0f03cde7279514193
KihadiMalick <malick.thiam@kihadi.com>
2026-02-27 02:32:49 +0200

    Delete app/error.tsx

--- app/error.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-export default function Error({ reset }: { error: Error; reset: () => void }) {
-  return (
-    <div style={{ padding: 40, textAlign: "center" }}>
-      <h1>Erreur</h1>
-      <button onClick={reset}>Réessayer</button>
-    </div>
-  );
-}
